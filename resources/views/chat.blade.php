bdc7cdd4fb22252bc38dd3fd96e2e1372b2165c8
Daniil Borovkov <daniilborowkow@yandex.ru>
2017-11-23 15:44:17 +0600

    Initial commit

---
+++ resources/views/chat.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Chat</title>
+  <link rel="stylesheet" href="css/app.css">
+</head>
+<body>
+  <h1>Chat room</h1>
+  <div id="app">
+    <example></example>
+  </div>
+
+  <script type="text/javascript" src="js/app.js"></script>
+</body>
+</html>
